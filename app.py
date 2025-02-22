6835eea5ce73076aea8471d288ca4c1c9cb24652
Roberto Bricia Bayos Jr. <145450481+CaseinBrt@users.noreply.github.com>
2025-02-22 23:53:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -251,10 +251,5 @@ def download_file(filename):
 def about():
     return render_template('about.html')
 
-@app.route('/')
-def home():
-    return "Hello, World!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
