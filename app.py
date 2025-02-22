83d313d71d38ce14441559a172e8e62adc24523c
Roberto Bricia Bayos Jr. <145450481+CaseinBrt@users.noreply.github.com>
2025-02-22 20:47:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -255,6 +255,7 @@ def about():
 def home():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    # Get port from environment variable or default to 10000
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
